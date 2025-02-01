d5486e7b7f02954479c6de5c259be01dde30fa5d
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-02-01 18:53:06 +0100

    test : check whether the coordinate fits the ship

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -3,10 +3,10 @@ import { createGameboard } from "../src/Gameboard";
 import { createShip } from "../src/Ship";
 
 describe('Gameboad module', () => {
-    describe('gameboard should able to place a ship at specific coordinate', () => {
+    describe('Gameboard should able to place a ship at specific coordinate.', () => {
+        const myBoard = createGameboard();
         test('check whether the coordinate had placed at', () => {
             const ship1 = createShip(5);
-            const myBoard = createGameboard();
             const coordinate = {x: 2,
                                 y: 2,
                                 direction: 'H'} // { horizental(H) or vertical(V) }
@@ -15,6 +15,25 @@ describe('Gameboad module', () => {
             const ship2 = createShip(2); 
             expect(myBoard.placeShip(ship2, coordinate)).toEqual(false);
 
+        })
+
+        test('Check whether the coordinate fits the ship\'s length.', () => {
+            const ship1 = createShip(5);
+            const fitCoordinate = {
+                x: 1,
+                y: 0,
+                direction: 'H' 
+            }
+
+            const wrongCoordinate = {
+                x: 6,
+                y: 0,
+                direction: 'H' 
+            } 
+
+            
+            expect(myBoard.placeShip(ship1, fitCoordinate)).toEqual(true);
+            expect(myBoard.placeShip(ship1, wrongCoordinate)).toEqual(false);
         })
     })
 })
